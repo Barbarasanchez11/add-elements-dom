2cac2d6a03f49d37060606457a64abf6390f4704
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2024-06-15 11:05:42 +0200

    Ejercicio Terminado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
- // Aquí tu código
+const boton = document.getElementById("agregar")
+const lista = document.getElementById("lista")
+
+function addElement(elemento) {
+    const nuevoItem = prompt("introduce un item")
+    if(nuevoItem){
+        let li = document.createElement("li")
+        li.textContent = nuevoItem
+        lista.appendChild(li)
+    } else { 
+        alert ("Escribe algo por favor")
+    }
+}
+
+boton.addEventListener("click", addElement)
+  
